4dbd70995137c3cb042b91a027a7d8193287bd99
Ivan <lolik1920@yandex.ru>
2021-03-30 21:03:05 +0500

    IP generator

---
+++ Generator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IP_generator
+{
+    class Generator
+    {
+        private int count;
+        public Generator(int count)
+        {
+            this.count = count;
+        }
+        public List<string> Generate()
+        {
+            Random rnd = new Random();
+            var IP = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                IP.Add(rnd.Next(0, 255) + "." + rnd.Next(0, 255) + "." + rnd.Next(0, 255) + "."+ rnd.Next(0, 255) + "."+ rnd.Next(0, 255));
+            }
+            return IP;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IP_generator
+{
+    class GenerateSomeIp
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите колиество генерируемых IP-адресов. От 1 до 100");
+                int countOfIp;
+                try
+                {
+                    countOfIp = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("Некоректный ввод.\n");
+                    continue;
+                }
+                countOfIp = countOfIp < 100 ? countOfIp : 100;
+                Generator gener = new Generator(countOfIp);
+                foreach (string ip in gener.Generate())
+                {
+                    Console.WriteLine(ip);
+                }
+            }
+        }
+    }
+}
